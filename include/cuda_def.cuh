832f725dbba8be90a926e4f173c4fae5c0dd4909
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-09-28 22:10:23 -0400

    basic comments before sdiv

--- include/cuda_def.cuh
+++ include/cuda_def.cuh
@@ -3,8 +3,14 @@
 
 #include <stdio.h>
 
-#define CUERR {
-    cudaError_t err;\
-    if((err = cudaGetLastError()) != cudaSuccess){\
-            printf("CUDA error : %s : %s, line %d\n", cudaGetErrorString(err), __FILE__, __LINE__); exit(1);}
+// Macro to check for CUDA errors
+#define CUERR {\
+    cudaError_t err; /* Variable to store the error code */\
+    if((err = cudaGetLastError()) != cudaSuccess){ /* Check if there was an error */\
+        /* Print the error message, file name, and line number, then exit */\
+        printf("CUDA error : %s : %s, line %d\n", cudaGetErrorString(err), __FILE__, __LINE__); \
+        exit(1);\
+    }\
 }
+
+#endif // CUDA_DEF_CUH
