034c0b8fe2b28ccb0dfe42a72a5578f45bd22c8f
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-09-29 18:38:00 -0400

    down sync

--- src/include/kernels/SHFL_FULLDTW_127.cuh
+++ src/include/kernels/SHFL_FULLDTW_127.cuh
@@ -25,4 +25,27 @@ void shfl_FullDTW_127(
     value_t penalty_here3 = INFINITY;
     value_t penalty_temp0;
     value_t penalty_temp1;
+
+    if(thid == 0){
+        penalty_left = INFINITY;
+        penalty_diag = INFINITY;
+        penalty_here0 = INFINITY;
+        penalty_here1 = INFINITY;
+        penalty_here2 = INFINITY;
+        penalty_here3 = INFINITY;
+    }
+
+    const value_t subject_value0 = 1 == 0?0:Subject[base+4*1-1];
+    const value_t subject_value1 = Subject[base+4*1-0];
+    const value_t subject_value2 = Subject[base+4*1+1];
+    const value_t subject_value3 = Subject[base+4*1+2];
+
+    index_t counter = 1;
+    value_t query_value = INFINITY;
+    value_t new_query_value = cQuery[thid];
+    if(thid == 0) query_value = new_query_value;
+    if(thid == 0) penalty_here1 = 0;
+    new_query_value = __shfl_down_sync(0xFFFFFFFF, new_query_value, 1, 32);
+    
+
     }
