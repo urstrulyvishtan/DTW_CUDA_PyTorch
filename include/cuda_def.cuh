10cde281c4d6e37c929b7c3e9008ed0dd8bae516
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-09-28 22:15:54 -0400

    start time and stop time for grids defined

--- include/cuda_def.cuh
+++ include/cuda_def.cuh
@@ -12,5 +12,19 @@
         exit(1);\
     }\
 }
+#define SDIV(x, y) ((x+y-1)/y)
 
+#define GRIDDIM (1024)
+#define BLOCKDIM (1024)
+
+#define STARTTIME cudaEvent_t Mstart, Mstop;\
+                  float Mtime;
+                  cudaEventCreate(&Mstart);\
+                  cudaEventCreate(&Mstop);\
+                  cudaEventRecord(Mstart, 0);
+                
+#define STOPTIME cudaEventRecord(Mstop, 0);\
+                 cudaEventSynchronize(Mstop);\
+                 cudaEventElapsedTime(&Mtime, Mstart, Mstop);\
+                 std::cout<<"Mtime:"<<Mtime<<std::endl;
 #endif // CUDA_DEF_CUH
